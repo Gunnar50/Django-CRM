83c3149e26a0f5662f74532a06d81a91363e737d
Gustavo <gustavo.passarella@hotmail.com>
2023-07-11 17:15:43 +0100

    login, logout, registration

--- website/views.py
+++ website/views.py
@@ -1,9 +1,13 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import authenticate, login, logout
 from django.contrib import messages
+from .forms import SignUpForm
+from .models import Record
 
 
 def home(request):
+    records = Record.objects.all()
+    
     # Check to see if loggin in
     if request.method == "POST":
         username = request.POST["username"]  # name="username"
@@ -20,13 +24,53 @@ def home(request):
             return redirect("home")
             
     else:
-        return render(request, "home.html", {})
+        return render(request, "home.html", {"records":records})
+
 
 def logout_user(request):
     logout(request)
     messages.success(request, "You have been logged out.")
     return redirect("home")
 
+
 def register_user(request):
-    return render(request, "register.html", {})
+    if request.method == "POST":
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # authenticate and login
+            username = form.cleaned_data["username"]  # whatever the user type in the form for username
+            password = form.cleaned_data["password1"]  # whatever the user type in the form for password
+            user = authenticate(request, username=username, password=password)
+            if user is not None:
+                login(request, user)
+                messages.success(request, "You have successfully registered and logged in")
+                return redirect("home")
+    else:
+        form = SignUpForm()
+        return render(request, "register.html", {"form": form})
     
+    return render(request, "register.html", {"form": form})
+    
+    
+def customer_record(request, pk):
+    if request.user.is_authenticated:
+        customer_record = Record.objects.get(id=pk)
+        
+        
+        return render(request, "record.html", {"customer_record": customer_record})
+    
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect("home")
+        
+        
+def delete_record(request, pk):
+    if request.user.is_authenticated:
+        delete = Record.objects.get(id=pk)
+        delete.delete()
+        messages.success(request, "Record deleted successfully")
+        return redirect("home")
+    else:
+        messages.success(request, "You must be logged in")
+        return redirect("home")
